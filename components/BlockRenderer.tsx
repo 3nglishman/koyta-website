6da59ce337ed8e3030b1b0f1fecb44419a95d23f
3nglishman <rolly@axiologyimports.com>
2026-01-23 23:24:39 -0800

    Update BlockRenderer.tsx

--- components/BlockRenderer.tsx
+++ components/BlockRenderer.tsx
@@ -4,36 +4,24 @@ import ImageBlock from "./blocks/ImageBlock";
 import HeroBlock from "./blocks/HeroBlock";
 import TextBlock from "./blocks/TextBlock";
 
-interface Block {
-  id: number;
-  block_type: string;
-  content: any;
-  sort: number | null;
-}
-
-export default function BlockRenderer({ block }: { block: Block }) {
+export default function BlockRenderer({ block }: { block: any }) {
   switch (block.block_type) {
     case "hero":
-      return <HeroBlock content={block.content} />;
+      return <HeroBlock {...block} />;
 
     case "text":
-      return <TextBlock content={block.content} />;
+      return <TextBlock {...block} />;
 
     case "image":
-      return <ImageBlock {...block.content} />;
+      return <ImageBlock {...block} />;
 
     case "icon_grid":
-      return <IconGridBlock {...block.content} />;
+      return <IconGridBlock {...block} />;
 
     case "cta":
-  return <CTABlock {...block.content} />;
+      return <CTABlock {...block} />;
 
     default:
-      return (
-        <pre style={{ padding: 20 }}>
-          Unsupported block type: {block.block_type}
-          {JSON.stringify(block, null, 2)}
-        </pre>
-      );
+      return null;
   }
 }
