de4728693dff7ffa337e65cb0352c9cd54d5441d
3nglishman <rolly@axiologyimports.com>
2026-01-24 10:56:51 -0800

    Update BlockRenderer.tsx

--- components/BlockRenderer.tsx
+++ components/BlockRenderer.tsx
@@ -1,29 +1,7 @@
-import HeroBlock from "./blocks/HeroBlock";
-import TextBlock from "./blocks/TextBlock";
-import ImageBlock from "./blocks/ImageBlock";
-import CTABlock from "./blocks/CTABlock";
-import IconGridBlock from "./blocks/IconGridBlock";
-
 export default function BlockRenderer({ block }: any) {
-  const { collection, item } = block;
-
-  switch (collection) {
-    case "hero_blocks":
-      return <HeroBlock content={item} />;
-
-    case "text_blocks":
-      return <TextBlock content={item} />;
-
-    case "image_blocks":
-      return <ImageBlock {...item} />;
-
-    case "cta_blocks":
-      return <CTABlock {...item} />;
-
-    case "icon_grid_blocks":
-      return <IconGridBlock {...item} />;
-
-    default:
-      return null;
-  }
+  return (
+    <pre style={{ padding: 20, background: "#fff" }}>
+      {JSON.stringify(block, null, 2)}
+    </pre>
+  );
 }
