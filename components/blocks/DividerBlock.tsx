c2b7f74ae72483d967b5ca952551c83db6d53491
3nglishman <rolly@axiologyimports.com>
2026-01-24 12:21:44 -0800

    Create DividerBlock.tsx

---
+++ components/blocks/DividerBlock.tsx
@@ -0,0 +1,7 @@
+export default function DividerBlock() {
+  return (
+    <section className="w-full py-8">
+      <hr className="max-w-6xl mx-auto border-t-2 border-gray-300" />
+    </section>
+  );
+}
