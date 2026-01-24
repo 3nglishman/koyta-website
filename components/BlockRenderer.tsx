1a61f2274115a47832f877aa2199fba3d094a693
3nglishman <rolly@axiologyimports.com>
2026-01-24 11:03:00 -0800

    Update BlockRenderer.tsx

--- components/BlockRenderer.tsx
+++ components/BlockRenderer.tsx
@@ -1,7 +1,37 @@
-export default function BlockRenderer({ block }: any) {
-  return (
-    <pre style={{ padding: 20, background: "#fff" }}>
-      {JSON.stringify(block, null, 2)}
-    </pre>
-  );
+import HeroBlock from "./blocks/HeroBlock";
+import TextBlock from "./blocks/TextBlock";
+import ImageBlock from "./blocks/ImageBlock";
+import IconGridBlock from "./blocks/IconGridBlock";
+import CTABlock from "./blocks/CTABlock";
+
+type M2ABlock = {
+  id: number;
+  collection: string;
+  item: any;
+};
+
+export default function BlockRenderer({ block }: { block: M2ABlock }) {
+  const data = block.item;
+
+  if (!data) return null;
+
+  switch (block.collection) {
+    case "hero_blocks":
+      return <HeroBlock content={data} />;
+
+    case "text_blocks":
+      return <TextBlock content={data} />;
+
+    case "image_blocks":
+      return <ImageBlock {...data} />;
+
+    case "icon_grid_blocks":
+      return <IconGridBlock {...data} />;
+
+    case "cta_blocks":
+      return <CTABlock {...data} />;
+
+    default:
+      return null;
+  }
 }
