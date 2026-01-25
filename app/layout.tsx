a83df61164a86167a73245c1e5d1ba01b1427bc1
3nglishman <rolly@axiologyimports.com>
2026-01-24 19:25:21 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,23 @@
-import Header from "./components/Header";
+import "./globals.css";
+import Header from "@/components/Header";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: "Koyta Hope & Rift Initiative",
+  description: "Where Love Is Sown, Hope Is Grown",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
+      <body className="w-full overflow-x-hidden bg-[#faf8f5]">
         <Header />
         {children}
       </body>
     </html>
   );
 }
+
