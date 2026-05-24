9f27103b0f7a4f4f5ea45192ca38b7ab7e9b6e2b
3nglishman <rolly@axiologyimports.com>
2026-05-24 15:39:42 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Script from 'next/script'
 
-export const metadata = {
-  title: "Koyta Hope & Rift Initiative",
-  description: "Where Love Is Sown, Hope Is Grown",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="w-full overflow-x-hidden bg-[#faf8f5]">
-        <Header />
+      <head />
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HZH5G7JQK2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HZH5G7JQK2');
+          `}
+        </Script>
         {children}
-        <Footer />
       </body>
     </html>
-  );
+  )
 }
